047cc717d41777d275a8b3b7ea60800b21f3df38
cintia.oliveira <cintia.oliveeira@hotmail.com>
2021-10-12 17:03:47 -0300

    validando e enviando formulário de cadastro

--- Repositories/CadastroRepository.cs
+++ Repositories/CadastroRepository.cs
@@ -9,13 +9,18 @@ namespace E_commerce.Repositories
 {
     public interface ICadastroRepository
     {
-
+        Cadastro Update(int cadastroId, Cadastro novoCadastro);
     }
     public class CadastroRepository : BaseRepository<Cadastro>, ICadastroRepository
     {
         public CadastroRepository(ApplicationContext contexto) : base(contexto)
         {
 
+        }
+
+        public Cadastro Update(int cadastroId, Cadastro novoCadastro)
+        {
+            throw new NotImplementedException();
         }
     }
 }
